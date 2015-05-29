d8d97bfd40a5904996e132652af1c269af8f20c0
ulises <ulises@tvc1.tvcnetwork.com>
2015-05-29 17:17:23 -0600

    Agregar completo

--- src/parqueo/baseDatos.java
+++ src/parqueo/baseDatos.java
@@ -7,27 +7,64 @@ package parqueo;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ulises
  */
 public class baseDatos {
-    public    Connection conn = null;
-    public    String sqlurl = "jdbc:mysql://localhost:3060/";
-    public    String driver = "com.mysql.jdbc.Driver";
-    public    String dbuser = "parqueo";
-    public    String dbpass = "r3p34r";
-    public    Statement st;
+    public static   Connection conn = null;
+    public static   String sqlurl = "jdbc:mysql://localhost:3306/";
+    public static   String databas = "parqueo?allowMultiQueries=true";
+    public static   String driver = "com.mysql.jdbc.Driver";
+    public static   String dbuser = "parqueo";
+    public static   String dbpass = "r3p34r";
+    public static   Statement st;
     
     
-    public static ResultSet ingresarAutos(){
-        ResultSet rs=null;
+    public static void ingresarAutos(String tipo, int posicion){
+        /*
+        Este metodo ingresa el carro al parqueo, y tambien inicia el registro
+        
+        */
+        // Para capturar el id del vehiculo de la db
+        int idVehiculo;
+        
+        //query
+        String consultasqlIn="INSERT INTO vehiculos(tipo,estado,posicion) VALUES('"+tipo+"',1,"+posicion+")";
         
         
-        return rs;
+        //tiempo
+        String Tiempo = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+        
+        String consultaPos="SELECT idVehiculo FROM vehiculos WHERE posicion="+posicion+" AND estado=1";
+        
+        
+        try{
+            Class.forName(driver).newInstance();
+            conn = (Connection) DriverManager.getConnection(sqlurl+databas,dbuser,dbpass);
+            st = (Statement) conn.createStatement();
+            st.executeUpdate(consultasqlIn);
+            st.close();
+            conn.close();  
+            
+            
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        
+        
+        
+        
     }
+    
+    
+    
     
     
     
